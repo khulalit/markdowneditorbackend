a181b9e614587e1ba72be982bc57f00e0f09063c
LalitKhu <lalit.khudhania@locofast.com>
2023-11-02 13:21:12 +0530

    cors error

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ mongoose.connect("mongodb+srv://Khulalit:Khulalit@markitdown.ewyfpbw.mongodb.net
 })
 .catch(err => console.log(err));
 app.use(cors({
-    origin: ['https://markitdownapp.netlify.app', 'http://localhost:3000', 'https://markitdowneditor.netlify.app/']
+    origin: ['*']
 }))
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
